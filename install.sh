10dacaabd2c4041d99839f5b062a89c564842f46
Yusuke Suzuki <suzukiyusuke@suzukiyuuyuunoMacBook-puro.local>
2018-09-01 13:05:28 +0900

    fix iroiro

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -s ~/dotfiles/.emacs.d ~/.emacs.d
+ln -s ~/dotfiles/.emacs.d ~/.emacs.
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 mkdir -p ~/.config/fish
 ln -s ~/dotfiles/config.fish ~/.config/fish/
